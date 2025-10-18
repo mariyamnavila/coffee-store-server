674261291528023f6417209ecb56af509e170d2c
mariyamnavila <bibimariyamnavila@gmail.com>
2025-10-18 16:52:18 +0600

    added find one method

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ async function run() {
             const result = await coffeesCollection.find().toArray();
             res.send(result)
         })
+        app.get('/coffees/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await coffeesCollection.findOne(query);
+            res.send(result)
+        })
 
         app.post('/coffees', async (req, res) => {
             const newCoffee = req.body
