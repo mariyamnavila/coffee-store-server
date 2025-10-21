293d2ce1abc54fb0d5895cb1f1b6d283ce2cbd72
mariyamnavila <bibimariyamnavila@gmail.com>
2025-10-21 17:30:38 +0600

    added find().toArray() method

--- index.js
+++ index.js
@@ -70,6 +70,11 @@ async function run() {
 
         // user related APIs
 
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result)
+        })
+
         app.post('/users', async (req, res) => {
             const userProfile = req.body;
             const result = await usersCollection.insertOne(userProfile);
